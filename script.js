58f1c1e187723b4cdf13cf3090225bc1946ff1fc
rynot-bit <rynotmkcrew@gmail.com>
2026-07-13 03:28:19 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function abrirSobre(){
 
+let sobre=document.querySelector(".sobre");
+let invitacion=document.querySelector(".invitacion");
+
+sobre.classList.add("abrir");
+
+setTimeout(function(){
+sobre.style.display="none";
+invitacion.classList.add("mostrar");
+},1000);
+
+}
