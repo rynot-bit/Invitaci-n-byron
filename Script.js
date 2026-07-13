4072560bc30eb0b424635ffc26b2bd8e3114339e
rynot-bit <rynotmkcrew@gmail.com>
2026-07-13 15:07:13 -0600

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,13 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-
-    const boton = document.getElementById("abrir");
-    const sobre = document.querySelector(".envelope");
-
-    if (boton && sobre) {
-        boton.onclick = function () {
-            sobre.classList.toggle("abierto");
-            alert("Sello presionado");
-        };
-    }
-
-});
+alert("SCRIPT FUNCIONANDO");
