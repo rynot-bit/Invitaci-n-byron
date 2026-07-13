d8e309d686df9ed70f7128f39c9fffb87e7c6d8e
rynot-bit <rynotmkcrew@gmail.com>
2026-07-13 15:31:15 -0600

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,28 @@
-alert("SCRIPT FUNCIONANDO");
+
+window.addEventListener("load", function () {
+
+    const loader = document.getElementById("loader");
+
+    if (loader) {
+        loader.style.display = "none";
+    }
+
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    const boton = document.getElementById("abrir");
+    const sobre = document.querySelector(".envelope");
+
+    if (boton && sobre) {
+
+        boton.addEventListener("click", function () {
+
+            sobre.classList.add("abierto");
+
+        });
+
+    }
+
+});
