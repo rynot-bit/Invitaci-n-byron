eac104b3120fb783688fb4e8fd59226751cfba89
rynot-bit <rynotmkcrew@gmail.com>
2026-07-13 15:53:54 -0600

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,14 +1,3 @@
 function abrirInvitacion(){
-
-    const sobre = document.querySelector(".envelope");
-    const contenido = document.getElementById("contenido");
-
-    if (sobre) {
-        sobre.classList.add("abierto");
-    }
-
-    if (contenido) {
-        contenido.style.display = "block";
-    }
-
+    alert("El botón funciona");
 }
