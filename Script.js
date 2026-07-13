5c2e24648e1bafb1e5cbe6ea951c176a35c17752
rynot-bit <rynotmkcrew@gmail.com>
2026-07-13 14:30:08 -0600

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,5 +1,4 @@
-window.addEventListener("load", function () {
-
-    document.body.classList.add("cargado");
-
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("principal").style.display = "block";
 });
